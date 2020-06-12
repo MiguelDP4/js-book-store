744bfcd076da23d76511d94c4d879579d209e1cc
migueldp4 <migueldp4@gmail.com>
2020-06-12 10:00:37 -0600

    add save method to book library

--- js/library.js
+++ js/library.js
@@ -1,16 +1,35 @@
 let myLibrary = [];
 
 class Book {
-  constructor(title, author, pages, read) {
+  constructor(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
   }
 }
 
+let getTitle = function() {
+  return document.getElementById('title-input').value;
+}
 
-function addBookToLibrary(title, author, pages, read) {
-  // do stuff here
-  myLibrary.push(new Book(title, author, pages, read));
+let getAuthor = function() {
+  return document.getElementById('author-input').value;
+}
+
+let getPages = function() {
+  return document.getElementById('pages-input').value;
+}
+
+function addBookToLibrary() {
+  myLibrary.push(new Book(getTitle(), getAuthor(), getPages()));
+  console.log(myLibrary);
+}
+
+let render = function(content, container){
+  container.innerHTML = content;
+}
+
+let writeSomething = function(text){
+  render(`<h2>${text}</h2>`, document.getElementById("book-container"));
 }
